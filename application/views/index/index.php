e78072728505f234009291a33ab67cf5c56b7c3c
Byron Peña <byronpenna@gmail.com>
2015-03-13 22:07:25 -0600

    elementos preliminares

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -2,9 +2,35 @@
 <html>
 <head>
 	<title>Mosaik</title>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/bootstrap/css/bootstrap.css") ?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/index/style.css") ?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/index/media.css") ?>>
 
 </head>
 <body>
-	<?php echo base_url() ?>
+	<div class='container-fluid main red'>
+		<div class="cuerpo col-lg-offset-3 col-lg-6 blue">
+			<div class="row">
+				<div class="col-lg-offset-3 col-lg-6 logoSection pink">
+					<img src=<?php echo base_url("resources/img/index/mosaikLogo.png")?> class='imgLogo'>			
+				</div>		
+			</div>
+			<div class="row">
+				<div class="container-fluid menuSection green">
+					<div class="col-lg-4 text-left">
+						<span>Corporate Image</span>
+					</div>
+					<div class="col-lg-4 text-center">
+						<span>Marketing Solutions</span>
+					</div>
+					<div class="col-lg-4 text-rigth">
+						<span>Web Development</span>
+					</div>
+				</div>	
+			</div>
+			
+		</div>
+		
+	</div>	
 </body>
 </html>
