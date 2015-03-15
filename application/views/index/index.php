387edc415774a19cd398a4cfa0ee2564f4f9cc6d
Byron Peña <byronpenna@gmail.com>
2015-03-15 12:00:32 -0600

    Slogan puesto

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -8,12 +8,17 @@
 	<meta name="viewport" content="width=device-width">
 </head>
 <body>
-	<div class='container-fluid main'>
+	<div class='container-fluid main todo'>
 		<div class="cuerpo col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
 			<div class="row">
-				<div class="col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6 col-xs-offset-3 col-xs-6 logoSection">
-					<img src=<?php echo base_url("resources/img/index/mosaikLogo.png")?> class='imgLogo'>			
-				</div>		
+				<div class="row">
+					<div class="col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6 col-xs-offset-3 col-xs-6 logoSection">
+						<img src=<?php echo base_url("resources/img/index/mosaikLogo.png")?> class='imgLogo'>			
+					</div>			
+				</div>
+				<div class="row sloganSection">
+					Making Your Ideas Awesomely Simple
+				</div>
 			</div>
 			<div class="row menuRow">
 				<div class="container-fluid menuSection ">
